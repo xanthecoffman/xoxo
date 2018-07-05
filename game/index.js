c26cd5805e0a747f70f8603a20d42a45f17a974a
Xanthe Coffman <xanthecoffman@gmail.com>
2018-07-05 13:06:44 -0400

    Hi

--- game/index.js
+++ game/index.js
@@ -1,6 +1,21 @@
-import {Map} from 'immutable'
+import { Map } from "immutable";
 
-export default function reducer(state, action) {
-  // TODO
-  return state
+let board = Map();
+
+export const move = (player = "X", position) => ({
+  type: "MOVE",
+  player,
+  position
+});
+
+export default function gameReducer(state = {}, action) {
+  console.log(action.player);
+  switch (action.player) {
+    case "X":
+      return { turn: "O", board: board.setIn(action.position, action.player) };
+    case "O":
+      return { turn: "X", board: board.setIn(action.position, action.player) };
+    default:
+      return state;
+  }
 }
--- index.js
+++ index.js
@@ -1,40 +1,44 @@
-import inquirer from 'inquirer'
+import inquirer from "inquirer";
+const { Map } = require("immutable");
+let board = Map();
 
-import gameReducer, {move} from './game'
-import {createStore} from 'redux'
+import gameReducer, { move } from "./game";
+import { createStore } from "redux";
 
 const printBoard = () => {
-  const {board} = game.getState()
+  const { board } = game.getState();
   for (let r = 0; r != 3; ++r) {
     for (let c = 0; c != 3; ++c) {
-      process.stdout.write(board.getIn([r, c], '_'))
+      process.stdout.write(board.getIn([r, c], "_"));
     }
-    process.stdout.write('\n')
+    process.stdout.write("\n");
   }
-}
+};
 
 const getInput = player => async () => {
-  const {turn} = game.getState()  
-  if (turn !== player) return
-  const ans = await inquirer.prompt([{
-    type: 'input',
-    name: 'coord',
-    message: `${turn}'s move (row,col):`
-  }])
-  const [row=0, col=0] = ans.coord.split(/[,\s+]/).map(x => +x)
-  game.dispatch(move(turn, [row, col]))
-}
+  const { turn } = game.getState();
+  if (turn !== player) return;
+  const ans = await inquirer.prompt([
+    {
+      type: "input",
+      name: "coord",
+      message: `${turn}'s move (row,col):`
+    }
+  ]);
+  const [row = 0, col = 0] = ans.coord.split(/[,\s+]/).map(x => +x);
+  game.dispatch(move(turn, [row, col]));
+};
 
 // Create the store
-const game = createStore(gameReducer)
+const game = createStore(gameReducer);
 
 // Debug: Print the state
 // game.subscribe(() => console.log(game.getState()))
 
-game.subscribe(printBoard)
-game.subscribe(getInput('X'))
-game.subscribe(getInput('O'))
+game.subscribe(printBoard);
+game.subscribe(getInput("X"));
+game.subscribe(getInput("O"));
 
 // We dispatch a dummy START action to call all our
 // subscribers the first time.
-game.dispatch({ type: 'START' })
+game.dispatch({ type: "START" });
